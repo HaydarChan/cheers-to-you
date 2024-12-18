4898127398b576331c13fe5955c88c400fb58c3c
HaydarChan <atqiyahaydar15@gmail.com>
2024-12-18 22:51:38 +0700

    first commit

---
+++ app/letter/page.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Cat from '@/public/cat.gif'
+import Image from 'next/image'
+
+const LetterPage = () => {
+  return (
+    <div className='mt-12 max-w-3xl px-4 w-full flex flex-col gap-y-4 items-center'>
+      <Image 
+        src={Cat}
+        height={150}
+        width={150}
+        alt='cat'
+      />
+      <p className='font-beanie text-2xl'>  
+        Hi, <br/>
+        You must be very busy right now, but thank you for taking the time to read this letter. <br/>
+        I just wanted to say that I’m wishing you good luck on your IELTS and exams. Remember this: all your hard work will pay off. <br/>
+        God is good; if you truly want something, He will help you achieve it. <br/>
+        And, you are precious. Every person who knows you believes in you—I believe in you, too. <br/>
+      </p>
+      <iframe className='rounded-[12px]' src="https://open.spotify.com/embed/track/0KyzXQhY2yzcb1FYCHkZc2?utm_source=generator&theme=0" width="100%" height="152" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+    </div>
+  )
+}
+
+export default LetterPage
---
+++ app/page.js
@@ -0,0 +1,47 @@
+"use client"
+
+// Library Import
+import Image from "next/image";
+import Link from "next/link";
+import { motion } from "framer-motion";
+
+// Image Import
+import Flower from "../public/flower.gif"
+
+// Component Import
+import { Button } from "@/components/ui/button";
+
+// Icons Import
+import { ArrowRight } from 'lucide-react';
+
+export default function Home() {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }} 
+      transition={{ duration: 1 }}
+      className="flex flex-col items-center mt-16 px-4 relative"
+    > 
+      <Image 
+        src={Flower}
+        alt="Flower"
+        unoptimized
+        height={256}
+        width={256}
+        className="mt-8"
+      />
+      <div className="flex flex-col gap-y-8">
+        <h1 className="text-2xl md:text-4xl text-gray-800 font-beanie">hi grace, <br /> this letter is for you.</h1>
+        <Link href={"/letter"} className="w-full flex justify-center">
+          <Button className="flex gap-x-4 hover:gap-x-6 transition-all">
+            See The Message <ArrowRight />
+          </Button>
+        </Link>
+      </div>
+      <div 
+        className="w-[250px] h-[250px] bg-pink-100 rounded-full backdrop-blur-0 absolute -z-20 filter blur-lg translate-y-12"
+      />
+    </motion.div>
+  );
+}
