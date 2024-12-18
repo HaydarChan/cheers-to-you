374f03c17944f2e275a6293f415bad50cb74ace5
HaydarChan <atqiyahaydar15@gmail.com>
2024-12-18 23:06:01 +0700

    feat: ya

--- app/letter/page.js
+++ app/letter/page.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 
 const LetterPage = () => {
   return (
-    <div className='mt-12 max-w-3xl px-4 w-full flex flex-col gap-y-4 items-center mb-20'>
+    <div className='mt-12 max-w-3xl px-4 w-full flex flex-col gap-y-8 items-center mb-20'>
       <Image 
         src={Cat}
         height={150}
@@ -12,11 +12,10 @@ const LetterPage = () => {
         alt='cat'
       />
       <p className='font-beanie text-xl md:text-2xl'>  
-        Hi, <br/>
-        You must be very busy right now, but thank you for taking the time to read this letter. <br/>
-        I just wanted to say that I’m wishing you good luck on your IELTS and exams. Remember this: all your hard work will pay off. <br/>
-        God is good; if you truly want something, He will help you achieve it. <br/>
-        And, you are precious. Every person who knows you believes in you—I believe in you, too. <br/>
+        Hi, You must be very busy right now, but thank you for taking the time to read this letter. 
+        I just wanted to say that I’m wishing you good luck on your IELTS and exams. Remember this: all your hard work will pay off.
+        God is good; if you truly want something, He will help you achieve it. 
+        And, you are precious. Every person who knows you believes in you—I believe in you, too.
       </p>
       <iframe className="rounded-[12px]" src="https://open.spotify.com/embed/track/5VBjyOQzqlPNgdRPMM6prF?utm_source=generator&theme=0" width="100%" height="152" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
     </div>
